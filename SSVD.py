e013c29c77ddf021224952c5aecd6f8790bef677
zahra dehghanian <z.dehghanian@aut.ac.ir>
2022-01-24 13:25:04 +0330

    change SSVD

--- SSVD.py
+++ SSVD.py
@@ -6,20 +6,20 @@ from scipy.linalg import svd
 def ssvd(z):
     print("SSVD start")
     zt = np.transpose(z)
-    s = []
-    v = []
-    for row in zt:
-        temp = []
-        for col in zt:
-            temp.append(np.matmul(row, col))
-        s.append(temp)
-    B, sigma = np.linalg.eig(s)
+    s = np.matmul(zt,z)
+    print(s.shape)
+    sigma,B = np.linalg.eig(s)
+    print(B.shape)
+    print(sigma.shape)
+    sigma = np.diag(sigma)
+    print(sigma.shape)
     sigma = fractional_matrix_power(sigma, 0.5)
     sigma_inverse = fractional_matrix_power(sigma, -1)
+    print(sigma_inverse.shape)
     R = np.matmul(B,sigma_inverse)
     A = np.matmul(z,R)
-    return show_result(z, A, sigma, B)
-
+    # return show_result(z, A, sigma, B)
+    return A , sigma ,B
 
 def show_result(z, A, sigma, B):
     # print("---------------------z is compute : " + str(z))
